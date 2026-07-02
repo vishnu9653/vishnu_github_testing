2b32b09299226824840c9a65b19e0b7a41734c43
vishnu9653 <vishanugurjar9653@gmail.com>
2026-07-02 14:37:31 +0530

    Remove unused variable 'x' from GithubTestingApplication

--- src/main/java/com/psa/GithubTestingApplication.java
+++ src/main/java/com/psa/GithubTestingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubTestingApplication {
-public int x=12;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GithubTestingApplication.class, args);
 	}
